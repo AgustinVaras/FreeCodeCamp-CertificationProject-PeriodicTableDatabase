9d17d978d0235cb48be3e6b87638e6e4fa70fa24
Agustin Varas <Agustin.Varas@outlook.com.ar>
2024-11-27 14:05:34 +0000

    feat: added PSQL variable for DB connection
    feat: Added first list try using variable SELECT_ELEMENT_RESULT
    
    feat: Added validation to check that SELECT_ELEMENT_RESULT is not NULL
    
    feat: Added first version of listing using execution parameter (Just with atomic_number for now)
    
    feat: Added validation to check if the execution parameter is a number or not
    
    feat: Added query searching for element symbol
    
    feat: added functional element search by symol and added validation for when it's not found
    
    feat: Added string length validation to discriminate between symbol and element's name searchs
    
    feat: added first version of element search by name
    
    fix: Added comentaries to clarify code functionality

--- element.sh
+++ element.sh
@@ -1,9 +1,45 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
 if [[ -z $1 ]]
 then
   #If no execution argument
   echo "Please provide an element as an argument."
-#else
-  #Execution logic
+else
+  #We hceck if the parameter is a number to know what type of query to use
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    SELECT_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1 " );
+
+    if [[ -z $SELECT_ELEMENT_RESULT ]]
+    then
+      echo "I could not find that element in the database."
+    else
+      echo $SELECT_ELEMENT_RESULT
+    fi
+  else
+    #If it's not a number we check parameter's length to valiate if it's a symbol or the element's name
+    if [[ ${#1} -le 2 ]]
+    then 
+      SELECT_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements WHERE symbol = '$1'" )
+
+      if [[ -z $SELECT_ELEMENT_RESULT ]]
+      then
+        echo "I could not find that element in the database."
+      else
+        echo $SELECT_ELEMENT_RESULT
+      fi
+    else
+      SELECT_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements WHERE name = '$1'" )
+
+      if [[ -z $SELECT_ELEMENT_RESULT ]]
+      then
+        echo "I could not find that element in the database."
+      else
+        echo $SELECT_ELEMENT_RESULT
+      fi
+    fi
+
+  fi  
 fi
