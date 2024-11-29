b62e179c5359b39cb1664a6bfd074201e071cd7b
Agustin Varas <Agustin.Varas@outlook.com.ar>
2024-11-29 14:04:12 +0000

    feat: Added JOIN logic to the atomic number SELECT
    feat: Added JOIN logic to symbol SELECT
    
    fix: added injection of the execution parameter for previous SELECTs logic
    
    feat: added JOIN logic for element name SELECT

--- element.sh
+++ element.sh
@@ -7,10 +7,30 @@ then
   #If no execution argument
   echo "Please provide an element as an argument."
 else
-  #We hceck if the parameter is a number to know what type of query to use
+  #We check if the parameter is a number to know what type of query to use
   if [[ $1 =~ ^[0-9]+$ ]]
   then
-    SELECT_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1 " );
+    SELECT_ELEMENT_RESULT=$($PSQL "
+      SELECT 
+        atomic_number, 
+        e.name, 
+        e.symbol, 
+        t.type, 
+        p.atomic_mass, 
+        p.melting_point_celsius, 
+        p.boiling_point_celsius 
+      FROM 
+        properties p 
+      INNER JOIN
+        elements e 
+        USING(atomic_number) 
+      INNER JOIN 
+        types t 
+      ON 
+        p.type_id = t.type_id
+      WHERE 
+        atomic_number = $1; " 
+    );
 
     if [[ -z $SELECT_ELEMENT_RESULT ]]
     then
@@ -22,7 +42,27 @@ else
     #If it's not a number we check parameter's length to valiate if it's a symbol or the element's name
     if [[ ${#1} -le 2 ]]
     then 
-      SELECT_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements WHERE symbol = '$1'" )
+      SELECT_ELEMENT_RESULT=$($PSQL "
+      SELECT 
+        atomic_number, 
+        e.name, 
+        e.symbol, 
+        t.type, 
+        p.atomic_mass, 
+        p.melting_point_celsius, 
+        p.boiling_point_celsius 
+      FROM 
+        properties p 
+      INNER JOIN
+        elements e 
+        USING(atomic_number) 
+      INNER JOIN 
+        types t 
+      ON 
+        p.type_id = t.type_id
+      WHERE 
+        e.symbol = '$1'; " 
+      ); 
 
       if [[ -z $SELECT_ELEMENT_RESULT ]]
       then
@@ -31,7 +71,27 @@ else
         echo $SELECT_ELEMENT_RESULT
       fi
     else
-      SELECT_ELEMENT_RESULT=$($PSQL "SELECT * FROM elements WHERE name = '$1'" )
+      SELECT_ELEMENT_RESULT=$($PSQL "
+        SELECT 
+          atomic_number, 
+          e.name, 
+          e.symbol, 
+          t.type, 
+          p.atomic_mass, 
+          p.melting_point_celsius, 
+          p.boiling_point_celsius 
+        FROM 
+          properties p 
+        INNER JOIN
+          elements e 
+          USING(atomic_number) 
+        INNER JOIN 
+          types t 
+        ON 
+          p.type_id = t.type_id
+        WHERE 
+          e.name = '$1'; " 
+      );
 
       if [[ -z $SELECT_ELEMENT_RESULT ]]
       then
